8103c58e2389b7ce7ffe141f2a6fad8b7103964a
santhoshdande09 <santhoshdande09@gmail.com>
2025-09-18 20:39:36 +0530

    Time: 20 ms (29.76%) | Memory: 58.3 MB (62.16%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int i;
+        Arrays.sort(nums);
+        for(i = 0; i < (nums.length) - 1; i++)
+        {
+            
+                if(nums[i] == nums[i+1])
+                {
+                    return true;
+                    
+                    
+                }
+            
+
+        }
+        return false;
+    }
+}
