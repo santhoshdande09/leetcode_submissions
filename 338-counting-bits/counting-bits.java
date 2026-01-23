6bfe7b2de2066023bdd016b09bbeaa76f7435a72
santhoshdande09 <santhoshdande09@gmail.com>
2026-01-23 14:29:41 +0530

    Time: 2 ms (96.48%) | Memory: 48.6 MB (59.59%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,20 +1,13 @@
 class Solution {
-    public static int setBit(int n)
-    {
-        int count = 0;
-        while(n > 0)
-        {
-            if((n & 1) != 0)
-            count++;
-            n = n >> 1;
-        }
-        return count;
-    }
     public int[] countBits(int n) {
         int ans[] = new int[n + 1];
-        for(int i = 0; i <= n; i++)
+        ans[0] = 0;
+        for(int i = 1; i <= n; i++)
         {
-            ans[i] = setBit(i);
+            if((i & 1) == 0)
+            ans[i] = ans[i/2];
+            else
+            ans[i] = ans[i/2] + 1;
         }
         return ans;
     }
