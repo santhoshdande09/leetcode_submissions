c114d91cb51c5c8dd20c1d3c10a6452b8901083d
santhoshdande09 <santhoshdande09@gmail.com>
2026-02-23 19:29:00 +0530

    Time: 1 ms (60.31%) | Memory: 49 MB (9.31%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,15 +1,33 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length, ans = 0, l = 0, r = n - 1, lmax = 0, rmax = 0;
-        while(l < r)
+        int n = height.length, ans = 0;
+        // while(l < r)
+        // {
+        //     lmax = Math.max(lmax, height[l]);
+        //     rmax = Math.max(rmax, height[r]);
+        //     if(lmax < rmax)
+        //         ans+= lmax - height[l++];
+        //     else
+        //     ans+= rmax - height[r--];
+        // }
+        int lmax[] = new int[n];
+        int rmax[] = new int[n];
+        lmax[0] = height[0];
+        rmax[n - 1] = height[n - 1];
+        for(int i = 1; i < n; i++)
         {
-            lmax = Math.max(lmax, height[l]);
-            rmax = Math.max(rmax, height[r]);
-            if(lmax < rmax)
-                ans+= lmax - height[l++];
-            else
-            ans+= rmax - height[r--];
+            lmax[i] = Math.max(lmax[i - 1], height[i]);
         }
+        for(int i = n - 2; i >= 0; i--)
+            rmax[i] = Math.max(rmax[i+1], height[i]);
+        for(int i = 0; i < n; i++)
+        {
+            if(lmax[i] < rmax[i])
+            ans += lmax[i] - height[i];
+            else
+            ans += rmax[i] - height[i];
+        }
+
         return ans;
     }
 }
