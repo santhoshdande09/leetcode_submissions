9ae86e27fdfe5258ac7050b205a380a29e7a7e10
santhoshdande09 <santhoshdande09@gmail.com>
2026-02-24 19:42:14 +0530

    Time: 1 ms (27.50%) | Memory: 42.9 MB (78.80%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        int n = s.length();
+        int absent = 0;
+        int currentLength= 1;
+        int maxLate = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(s.charAt(i) == 'A')
+                absent++;
+        }
+         for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) =='L' && s.charAt(i - 1) == 'L')
+                currentLength++;
+                 else
+                currentLength = 1;
+            if (currentLength > maxLate)
+                maxLate = currentLength;
+        }
+        if(absent < 2 && maxLate < 3)
+            return true;
+            return false;
+    }
+}
