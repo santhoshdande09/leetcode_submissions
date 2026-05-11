dde709f9b17d9a15f591cd2fd05c9950bf29eb85
santhoshdande09 <santhoshdande09@gmail.com>
2026-05-11 21:06:28 +0530

    Time: 5 ms (77.69%) | Memory: 46.6 MB (65.54%) - LeetSync

---
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] separateDigits(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int num : nums) {
+            String s = String.valueOf(num);
+            for (char c : s.toCharArray())
+                result.add(c - '0');
+        }
+        int[] ans = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            ans[i] = result.get(i);
+        }
+        return ans;
+    }
+}
