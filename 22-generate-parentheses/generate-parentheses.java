93a178966bc9f601ae67009cc59e24ae6ba753f7
santhoshdande09 <santhoshdande09@gmail.com>
2026-08-17 19:20:22 +0530

    Time: 2 ms (70.08%) | Memory: 44.8 MB (44.70%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        solve(ans, "", 0, 0, n);
+        return ans;
+    }
+
+    void solve(List<String> ans, String s, int open, int close, int n) {
+        if (s.length() == 2 * n) {
+            ans.add(s);
+            return;
+        }
+
+        if (open < n)
+            solve(ans, s + "(", open + 1, close, n);
+
+        if (close < open)
+            solve(ans, s + ")", open, close + 1, n);
+    }
+}
